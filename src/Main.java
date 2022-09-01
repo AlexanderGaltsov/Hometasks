a8fd1b6f5caf4845d65287869df61eaa2332e2c7
AlexanderGaltsov <gau051177@gmail.com>
2022-09-01 04:34:54 +0300

    Добавлен расчет обрезанного среднего

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SalesManager salesManager = new SalesManager(new int[]{30, 12, 26, 85, 52, 56, 95, 214,});
+        SalesManager salesManager = new SalesManager(new int[]{30, 12, 214, 85, 12, 56, 12, 214,});
 
         System.out.println("Максимальное число продаж " + salesManager.max());
+        System.out.println("Расчет обрезанного среднего продаж " + salesManager.averageTrimmedSales());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,40 @@ public class SalesManager {
         }
         return max;
     }
+    public int min() {
+        int min = sales[0];
+        for (int sale : sales)  {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+    public int highsNumber() {
+        int highsNumber = 0;
+        for (int sale : sales) {
+            if (sale == max()) {
+                highsNumber += 1;
+            }
+        }
+        return highsNumber;
+    }
+    public int lowsNumber() {
+        int lowsNumber = 0;
+        for (int sale : sales) {
+            if (sale == min()) {
+                lowsNumber += 1;
+            }
+        }
+        return lowsNumber;
+    }
+    public int averageTrimmedSales() {
+        int sum = 0;
+        for(int sale : sales) {
+            sum += sale;
+        }
+        return
+                (sum - (max() * highsNumber()) - (min() * lowsNumber()))
+                        / (sales.length - highsNumber() - lowsNumber());
+    }
 }
